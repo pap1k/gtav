762d72ea87be362037b2a7da23969b5bf71ae8c6
pap1k <35275000+pap1k@users.noreply.github.com>
2021-07-13 22:43:49 +0300

    дохуя чего сделал по мелочи

--- client_packages/clientmode/index.js
+++ client_packages/clientmode/index.js
@@ -2,16 +2,28 @@ let browser
 mp.events.add('showLogin', ()=>{
     browser = mp.browsers.new("package://browser/auth/login.html")
     mp.gui.chat.activate(false)
-    setTimeOut(()=>{browser.execute("mp.invoke('focus', true)")}, 500)
+    setTimeout(()=>{
+        mp.gui.cursor.show(true, true)
+    }, 0)
 })
 mp.events.add('showReg', ()=>{
     browser = mp.browsers.new("package://browser/auth/reg.html")
+    mp.gui.chat.activate(false)
+    setTimeout(()=>{
+        mp.gui.cursor.show(true, true)
+    }, 0)
 })
 
-mp.events.add('sendLogin', (data)=>{
+mp.events.add('sendLogin', data =>{
     mp.events.callRemote('onPlayerLogin', data)
 })
 
-mp.events.add('sendReg', (data)=>{
+mp.events.add('sendReg', data =>{
     mp.events.callRemote('onPlayerRegister', data)
+})
+
+mp.events.add('hideAllBrowsers', ()=> {
+    mp.gui.cursor.show(false, false)
+    mp.gui.chat.activate(true)
+    browser.destroy()
 })
--- packages/trinity-rpg/index.js
+++ packages/trinity-rpg/index.js
@@ -1,13 +1,14 @@
 //init Commands file
 require('./commands/main')
 require("./reg-log")
+const db = require("./server-side-conf.json").db
 //const db = require('./db')
 const mongoose = require("mongoose")
 const utils = require("./utils")
 async function start(){
     try{
         await mongoose.connect(
-            'mongodb+srv://papercut:A2XpDsrRuE7rHgX@gtavtest.j8y3y.mongodb.net/gtav',
+            `mongodb+srv://${db.user}:${db.password}@gtavtest.j8y3y.mongodb.net/gtav`,
             {
                 useNewUrlParser: true,
                 useFindAndModify: false,
