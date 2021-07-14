08a1c5a70b02b840a5c97affce0cf3f933f3035a
pap1k <35275000+pap1k@users.noreply.github.com>
2021-07-14 12:36:01 +0300

    added /unmute

--- packages/trinity-rpg/commands/mute.js
+++ packages/trinity-rpg/commands/mute.js
@@ -18,8 +18,22 @@ exports.obj = [
                 return player.outputChatBox("Время должно быть числом")
             
             targ.setVariable('muted', {state: true, muteTimeStamp: Date.now(), muteDuration: time, reason})
-            targ.outputChatBox(`Администратор ${player.name} выжал вам мут на ${time} минут. Причина: ${reason}`)
+            targ.outputChatBox(`Администратор ${player.name} выдал вам мут на ${time} минут. Причина: ${reason}`)
             sendToAdmins(`Администратор ${player.name} выдал мут игроку ${targ.name} на ${time} минут. Причина: ${reason}`)
+        }
+    },
+    {
+        triggers: ["unmute"],
+        lvl: lvls.ALL_ADMINS,
+        target: true,
+        hint: "/unmute [id или часть ника]",
+        execute: (player, _, targ) => {
+            if(!targ.getVariable('muted'))
+                return targ.outputChatBox("Указанный игрок не находится в муте")
+            
+            targ.setVariable('muted', false)
+            targ.outputChatBox(`Администратор ${player.name} снял вам мут`)
+            sendToAdmins(`Администратор ${player.name} снял мут игроку ${targ.name}`)
         }
     }
 ]
