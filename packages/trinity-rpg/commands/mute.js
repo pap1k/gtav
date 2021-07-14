4a7f6bf68fce507a1b93cd538b05d43384cc6275
pap1k <35275000+pap1k@users.noreply.github.com>
2021-07-14 12:31:46 +0300

    Доделана основа /mute

--- packages/trinity-rpg/commands/mute.js
+++ packages/trinity-rpg/commands/mute.js
@@ -1,5 +1,6 @@
+const { MongoTimeoutError } = require("mongodb")
 const lvls = require("../lvls")
-const {findPlayerByIdOrNickname, sendToAdmins} = require("../utils")
+const {sendToAdmins} = require("../utils")
 var exports = module.exports = {}
 
 exports.obj = [
@@ -13,6 +14,12 @@ exports.obj = [
             if(targ.getVariable('muted'))
                 return player.outputChatBox("Указанный игрок уже находится в муте")
 
+            if(!parseInt(time))
+                return player.outputChatBox("Время должно быть числом")
+            
+            targ.setVariable('muted', {state: true, muteTimeStamp: Date.now(), muteDuration: time, reason})
+            targ.outputChatBox(`Администратор ${player.name} выжал вам мут на ${time} минут. Причина: ${reason}`)
+            sendToAdmins(`Администратор ${player.name} выдал мут игроку ${targ.name} на ${time} минут. Причина: ${reason}`)
         }
     }
 ]
