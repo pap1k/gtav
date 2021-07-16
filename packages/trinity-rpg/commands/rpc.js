fdb95766156fccbc456c7027001411a1e49b99c0
pap1k <35275000+pap1k@users.noreply.github.com>
2021-07-16 23:04:39 +0300

    Create rpc.js

---
+++ packages/trinity-rpg/commands/rpc.js
@@ -0,0 +1,30 @@
+const lvls = require("../lvls")
+const {sendToAdmins} = require("../utils")
+const Player = require("../db_models/Player")
+var exports = module.exports = {}
+exports.obj = [
+    {
+        triggers: ["rpc"],
+        lvl: lvls.ALL_ADMINS,
+        execute: (player) => {
+            if (player.vehicle)
+                player.vehicle.repair()
+            else
+                player.outputChatBox("Вы должны находиться в транспорте")
+        }    
+    },
+    {
+        triggers: ["rcar", "repaircar"],
+        lvl: lvls.ALL_ADMINS,
+        hint: "/rcar [id или часть ника]",
+        target: true,
+        execute: (player, _, target) => {
+            if(target.vehicle){
+                target.vehicle.repair()
+                sendToAdmins(`Администратор ${player.name} отремонтировал транспорт игрока ${target.name}`)
+            }
+            else
+                player.outputChatBox("Указанный вами игрок не находится в транспорте")
+        }
+    }
+]
